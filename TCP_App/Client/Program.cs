e5dcb94936cedc8dc21003a1bdc3c1ee593d136a
Fineas Pop <fineaspop@gmail.com>
2025-03-30 11:54:12 +0200

    made the server and single client communication.

---
+++ TCP_App/Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Sockets;
+
+
+Console.WriteLine("Enter Server IP");
+string serverIP=Console.ReadLine()??string.Empty;
+
+try
+{
+	TcpClient client = new TcpClient(serverIP, 5000);
+	Console.WriteLine("Connected to server.");
+
+	NetworkStream stream = client.GetStream();
+	StreamReader reader = new StreamReader(stream);
+	StreamWriter writer = new StreamWriter(stream) { AutoFlush=true};
+
+	while (true)
+	{
+		Console.Write("You: ");
+		string message=Console.ReadLine()??string.Empty;
+
+		if(string.IsNullOrEmpty(message))
+			break;
+
+		writer.WriteLine(message);
+		string response=reader.ReadLine()??string.Empty;
+		Console.WriteLine($"Server: {response}");
+	}
+}
+catch(Exception ex)
+{
+	Console.WriteLine($"Error: {ex}");
+}
---
+++ TCP_App/Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+TcpListener listener = new TcpListener(IPAddress.Any, 5000);
+listener.Start();
+Console.WriteLine($"Server started. Waiting for clients ...");
+
+TcpState state = new TcpState();
+Console.WriteLine(state.ToString());
+
+TcpClient client = listener.AcceptTcpClient();
+Console.WriteLine("Client connected!");
+
+NetworkStream stream = client.GetStream();
+StreamReader reader = new StreamReader(stream);
+StreamWriter writer = new StreamWriter(stream) {AutoFlush=true };
+
+try
+{
+	while (true)
+	{
+		string message=reader.ReadLine() ?? string.Empty;
+		if(string.IsNullOrEmpty(message))
+			break;
+
+		Console.WriteLine($"Client: {message}");
+		writer.WriteLine("Server received: "+message);
+	}
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"Error: {ex}");
+}
+finally
+{
+	client.Close();
+	listener.Stop();
+	Console.WriteLine("Server shutdown");
+}
